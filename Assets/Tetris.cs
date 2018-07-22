0a435f1cc30df1546ec0a9bd8e35a96264076b92
aabrosov <aabrosov@rs.com>
2018-07-22 19:05:22 +0500

    many changes

--- Assets/Tetris.cs
+++ Assets/Tetris.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System.Collections;
+
 namespace Tetris
 {
     public class Select
     {
-        public static int Figure()
+        public static Figure Figure()
         {
             float rnd = Random.Range(0.0f, 100.0f);
             float LimitLeft = 0.0f;
@@ -13,12 +15,12 @@ namespace Tetris
             {
                 LimitLeft = LimitRight;
                 LimitRight = LimitLeft + Tetris.Figures[i].probability;
-                if (rnd > LimitLeft & rnd < LimitRight)
+                if (rnd >= LimitLeft & rnd <= LimitRight)
                 {
-                    return i;
+                    return Tetris.Figures[i];
                 }
             }
-            return -1;
+            return null;
         }
     }
     public class Figure
@@ -26,24 +28,16 @@ namespace Tetris
         public Color color;
         public int[,] tiles;
         public int probability;
-        public Figure(Color color, int[,] tiles, int probability)
+        public bool allowrotate;
+        public int positionx;
+        public int positiony;
+        public Figure() { }
+        public Figure(Color color, int[,] tiles, int probability, bool allowrotate)
         {
             this.color = color;
             this.tiles = tiles;
             this.probability = probability;
-        }
-    }
-    public class Position
-    {
-        public int x;
-        public int y;
-        public int r;
-        public Position() { }
-        public Position(int x, int y, int r)
-        {
-            this.x = x;
-            this.y = y;
-            this.r = r;
+            this.allowrotate = allowrotate;
         }
     }
     public class Tetris : MonoBehaviour
@@ -52,33 +46,26 @@ namespace Tetris
         private static int ScreenHeight;
         private static int GlassWidth;
         private static int GlassHeight;
-        private static int[,] Glass;
-        private static int[,] Figure;
-        private static int[,] Moved;
+        private static Color[,] Glass;
+        private static int GameMode;
         private static int Scale;
-        private static int CurrentFig;
+        private static Figure CurrentFig = new Figure();
         //private static int PosX;
         //private static int PosY;
-        private static Position before = new Position();
+        //private static Position position = new Position();
         public static Figure[] Figures = {
-                new Figure( new Color(1.0f,0.0f,0.0f), new int[,] {
-                    {0,0,0,0},
-                    {0,1,1,0},
-                    {0,1,1,0},
-                    {0,0,0,0}
-                }, 10 ),
-                new Figure( new Color(0.0f,1.0f,0.0f), new int[,] {{0,0,0,0},{0,0,1,0},{0,1,1,0},{0,1,0,0}}, 15 ),
-                new Figure( new Color(0.0f,0.0f,1.0f), new int[,] {{0,0,0,0},{0,1,0,0},{0,1,1,0},{0,0,1,0}}, 15 ),
-                new Figure( new Color(0.0f,1.0f,1.0f), new int[,] {{0,0,0,0},{0,1,1,0},{0,0,1,0},{0,0,1,0}}, 15 ),
-                new Figure( new Color(1.0f,0.0f,1.0f), new int[,] {{0,0,0,0},{0,0,1,0},{0,0,1,0},{0,1,1,0}}, 15 ),
-                new Figure( new Color(1.0f,1.0f,0.0f), new int[,] {{0,0,1,0},{0,0,1,0},{0,0,1,0},{0,0,1,0}}, 10 ),
-                new Figure( new Color(0.5f,1.0f,1.0f), new int[,] {{0,0,0,0},{0,0,1,0},{0,1,1,0},{0,0,1,0}}, 20 ),
-                new Figure( new Color(1.0f,0.5f,1.0f), new int[,] {{0,0,0,0},{0,1,0,0},{1,1,1,0},{0,1,0,0}},  5 ),
-                new Figure( new Color(1.0f,1.0f,0.5f), new int[,] {{0,0,0,0},{0,1,1,0},{0,1,0,0},{0,1,1,0}},  5 ),
-                new Figure( new Color(1.0f,0.5f,0.5f), new int[,] {{0,0,0,0},{0,0,0,1},{0,0,1,1},{0,1,1,0}},  5 )
-            };
+            new Figure(new Color(1.0f, 0.0f, 0.0f), new int[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 }, { 1, 1 } }, 10, false),
+            new Figure(new Color(0.0f, 1.0f, 0.0f), new int[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, {-1, 1 }, {-1, 1 } }, 15, true),
+            new Figure(new Color(0.0f, 0.0f, 1.0f), new int[,] { { 0, 0 }, { 0, 1 }, { 1, 1 }, {-1, 0 }, {-1, 0 } }, 15, true),
+            new Figure(new Color(0.0f, 1.0f, 1.0f), new int[,] { { 0, 0 }, { 1, 0 }, {-1, 0 }, { 1, 1 }, { 1, 1 } }, 15, true),
+            new Figure(new Color(1.0f, 0.0f, 1.0f), new int[,] { { 0, 0 }, { 1, 0 }, {-1, 0 }, {-1, 1 }, {-1, 1 } }, 15, true),
+            new Figure(new Color(1.0f, 1.0f, 0.0f), new int[,] { { 2, 0 }, { 1, 0 }, { 0, 0 }, {-1, 0 }, {-1, 0 } }, 10, true),
+            new Figure(new Color(0.8f, 0.0f, 0.0f), new int[,] { { 0, 0 }, { 1, 0 }, { 0, 1 }, {-1, 0 }, {-1, 0 } }, 20, true),
+            new Figure(new Color(0.0f, 0.8f, 0.0f), new int[,] { { 0, 0 }, { 0, 1 }, { 0,-1 }, { 1, 0 }, {-1, 0 } }, 5, false),
+            new Figure(new Color(0.0f, 0.0f, 0.8f), new int[,] { { 0, 0 }, { 1, 0 }, {-1, 0 }, { 1, 1 }, {-1, 1 } }, 5, true),
+            new Figure(new Color(0.0f, 0.8f, 0.8f), new int[,] { { 0, 0 }, { 1,-1 }, { 0,-1 }, {-1, 0 }, {-1, 1 } }, 5, true),
+        };
         public static int FigCount;
-        public Texture2D[] MyTextures;
         // Use this for initialization
         void Start()
         {
@@ -86,51 +73,60 @@ namespace Tetris
             ScreenHeight = Screen.height;
             if (EditorUtility.DisplayDialog("Game mode selection", "Please, select game mode", "Mode 1", "Mode 2"))
             {
-                //GameMode 1
+                GameMode = 1;
+            }
+            else
+            {
+                GameMode = 2;
+            }
+            if (GameMode == 1)
+            {
                 GlassHeight = 20;
                 GlassWidth = 10;
                 FigCount = 7;
             }
-            else
+            else if (GameMode == 2)
             {
-                //GameMode 2
                 GlassHeight = 12;
                 GlassWidth = 20;
                 FigCount = 10;
                 Figures[6].probability = 5;
             }
-            //define and fill textures
-            MyTextures = new Texture2D[FigCount];
-            for (int i = 0; i < FigCount; i++)
-            {
-                MyTextures[i] = new Texture2D(1, 1);
-                MyTextures[i].SetPixel(0, 0, Figures[i].color);
-                MyTextures[i].Apply();
-            }
-            int ScaleX = ScreenWidth / GlassWidth;
-            int ScaleY = ScreenHeight / GlassHeight;
+            int ScaleX = ScreenWidth / (GlassWidth + 2);
+            int ScaleY = ScreenHeight / (GlassHeight + 1);
             Scale = Mathf.Min(ScaleX, ScaleY);
-            Glass = new int[GlassWidth, GlassHeight];
-            Position position = new Position(GlassWidth / 2 - 2, 0, 0);
+            Glass = new Color[GlassWidth, GlassHeight];
             CurrentFig = Select.Figure();
-            InitFigure(position, CurrentFig);
+            CurrentFig.positionx = GlassWidth / 2;
+            CurrentFig.positiony = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                Glass[CurrentFig.tiles[i, 0] + CurrentFig.positionx, CurrentFig.tiles[i, 1] + CurrentFig.positiony] = CurrentFig.color;
+            }
         }
         void OnGUI()
         {
-            //draw Glass and Figure
-            for (int i = 0; i < GlassWidth; i++)
+            Color currentcolor;
+            //redraw Glass with walls and bottom
+            for (int i = -1; i < GlassWidth + 1; i++)
             {
-                for (int j = 0; j < GlassHeight; j++)
+                for (int j = 0; j < GlassHeight + 1; j++)
                 {
-                    if (Glass[i, j] != 0)
+                    if (i == -1 | i == GlassWidth | j == GlassHeight)
                     {
-                        GUI.skin.box.normal.background = MyTextures[Glass[i, j]];
-                        GUI.Box(new Rect(i * Scale, j * Scale, Scale, Scale), "");
+                        currentcolor = Color.black;
                     }
-                    if (Figure[i, j] != 0)
+                    else
                     {
-                        GUI.skin.box.normal.background = MyTextures[CurrentFig];
-                        GUI.Box(new Rect(i * Scale, j * Scale, Scale, Scale), "");
+                        currentcolor = Glass[i, j];
+                    }
+                    if (currentcolor != Color.white)
+                    {
+                        Texture2D MyTexture = new Texture2D(1, 1);
+                        MyTexture.SetPixel(0, 0, currentcolor);
+                        MyTexture.Apply();
+                        GUI.skin.box.normal.background = MyTexture;
+                        GUI.Box(new Rect((i + 1) * Scale, j * Scale, Scale, Scale), "");
                     }
                 }
             }
@@ -138,65 +134,66 @@ namespace Tetris
         // Update is called once per frame
         void Update()
         {
-            //Moved = new int[GlassWidth, GlassHeight];
-            Position after = new Position();
-            after = before;
-            if (Input.GetKeyDown("up"))
-                after.r += 90;
+            for (int i = 0; i < 5; i++)
+            {
+                Glass[CurrentFig.tiles[i, 0] + CurrentFig.positionx, CurrentFig.tiles[i, 1] + CurrentFig.positiony] = Color.white;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+                CurrentFig = RotateLeft(CurrentFig);
             else if (Input.GetKeyDown("a"))
-                after.r -= 90;
+                CurrentFig = RotateRight(CurrentFig);
             else if (Input.GetKeyDown("d"))
-                after.r += 90;
+                CurrentFig = RotateLeft(CurrentFig);
             else if (Input.GetKeyDown("down"))
-                after.y += 1;
+                CurrentFig.positiony += 1;
             else if (Input.GetKeyDown("left"))
-                after.x -= 1;
+                CurrentFig.positionx -= 1;
             else if (Input.GetKeyDown("right"))
-                after.x += 1;
-            InitFigure(after, CurrentFig);
-        }
-        void InitFigure(Position pos, int CurrentFig)
-        {
-            Figure = new int[GlassWidth, GlassHeight];
-            for (int i = 0; i < 4; i++)
+                CurrentFig.positionx += 1;
+            for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j < 4; j++)
+                Glass[CurrentFig.tiles[i, 0] + CurrentFig.positionx, CurrentFig.tiles[i, 1] + CurrentFig.positiony] = CurrentFig.color;
+            }
+            //System.Threading.Thread.Sleep(5000);
+            //DropFigure();
+        }
+        //void DropFigure()
+        //{
+        //    dropped = new Color[GlassWidth, GlassHeight];
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        dropped[CurrentFig.tiles[i, 0] + CurrentFig.positionx, CurrentFig.tiles[i, 1] + CurrentFig.positiony] = CurrentFig.color;
+        //    }
+        //}
+        Figure RotateLeft(Figure figure)
+        {
+            if (figure.allowrotate)
+            {
+                int x, y;
+                for (int i = 0; i < 5; i++)
                 {
-                    Figure[i + pos.x, j + pos.y] = Rotated(pos.r, GetFigure(CurrentFig))[i,j];
+                    x = +figure.tiles[i, 1];
+                    y = -figure.tiles[i, 0];
+                    figure.tiles[i, 0] = x;
+                    figure.tiles[i, 1] = y;
                 }
             }
+            return figure;
         }
-        int[,] GetFigure(int CurrentFig)
+        Figure RotateRight(Figure figure)
         {
-            int[,] Figure = new int[4, 4];
-            for (int i = 0; i < 4; i++)
+            if (figure.allowrotate)
             {
-                for (int j = 0; j < 4; j++)
+                int x, y;
+                for (int i = 0; i < 5; i++)
                 {
-                    Figure[i, j] = Figures[CurrentFig].tiles[i, j];
+                    x = -figure.tiles[i, 1];
+                    y = +figure.tiles[i, 0];
+                    figure.tiles[i, 0] = x;
+                    figure.tiles[i, 1] = y;
                 }
             }
-            return Figure;
-        }
-        int[,] Rotated(int angle, int[,] figure)
-        {
-            angle = angle % 360;
-            if (angle == 0)
-                return figure;
-            int [,] Temp = new int[4, 4];
-            if (angle == 90)
-                for (int i = 0; i < 4; i++)
-                    for (int j = 0; j < 4; j++)
-                        Temp[i, j] = figure[j, 3 - i];
-            if (angle == 180)
-                for (int i = 0; i < 4; i++)
-                    for (int j = 0; j < 4; j++)
-                        Temp[i, j] = figure[3 - i, 3 - j];
-            if (angle == 270)
-                for (int i = 0; i < 4; i++)
-                    for (int j = 0; j < 4; j++)
-                        Temp[i, j] = figure[3 - j, i];
-            return Temp;
+            return figure;
         }
     }
 }
